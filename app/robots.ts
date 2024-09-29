01452618f3e1a04e150ae97257484eb98806425c
Aamir Azad <aamirmazad@gmail.com>
2024-09-29 10:40:35 -0400

    feat: block api bots

--- app/robots.ts
+++ app/robots.ts
@@ -4,7 +4,40 @@ export default function robots() {
   return {
     rules: [
       {
+        userAgent: 'GPTBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'ChatGPT-User',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Google-Extended',
+        disallow: '/',
+      },
+      {
+        userAgent: 'CCBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'anthropic-ai',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Claude-Web',
+        disallow: '/',
+      },
+      {
+        userAgent: 'ClaudeBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'PerplexityBot',
+        disallow: '/',
+      },
+      {
         userAgent: '*',
+        allow: '/',
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
