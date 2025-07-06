a43d52656fe09d2a1ee60af3b8bb529c03ec5a87
Aamir Azad <aamirmazad@gmail.com>
2025-07-06 11:01:21 -0400

    lg-laptop: 2025-07-06 11:01:21

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { Navbar } from "./components/nav";
-import { Analytics } from "@vercel/analytics/next";
 import Footer from "./components/footer";
 
 export const metadata: Metadata = {
